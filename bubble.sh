bca5c8322e2f922bae35a353fab9a20b7a599703
Melvin Carvalho <melvincarvalho@gmail.com>
2016-01-24 10:50:33 +0100

    buble twice

--- bubble.sh
+++ bubble.sh
@@ -4,11 +4,11 @@ export WEBID="http://melvincarvalho.com/#me"
 export WORKBOT="https://workbot.databox.me/profile/card#me"
 export SMALL="https://localhost/wallet/small/wallet#this"
 export MEDIUM="https://localhost/wallet/medium/wallet#this"
-THRESHOLD=25
+THRESHOLD=30
 AMOUNT=5
 credit insert $WORKBOT $AMOUNT '' $WEBID work -d small -w $SMALL
 BALANCE=$(credit balance $WEBID -d small -w $SMALL)
-if [[ $BALANCE -gt $THRESHOLD ]]
+if [[ $BALANCE -ge $THRESHOLD ]]
 then
   credit insert $WEBID $THRESHOLD '' $WORKBOT bubble -d small -w $SMALL
   credit insert $WORKBOT $THRESHOLD '' $WEBID cycle -d medium -w $MEDIUM
---
+++ bubbletwice.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+export WEBID="http://melvincarvalho.com/#me"
+export WORKBOT="https://workbot.databox.me/profile/card#me"
+export SMALL="https://localhost/wallet/small/wallet#this"
+export MEDIUM="https://localhost/wallet/medium/wallet#this"
+export LARGE="https://localhost/wallet/medium/wallet#this"
+
+THRESHOLD1=30
+THRESHOLD1=300
+AMOUNT=5
+
+credit insert $WORKBOT $AMOUNT '' $WEBID work -d small -w $SMALL
+BALANCE=$(credit balance $WEBID -d small -w $SMALL)
+if [[ $BALANCE -ge $THRESHOLD1 ]]
+then
+  credit insert $WEBID $THRESHOLD1 '' $WORKBOT bubble -d small -w $SMALL
+  credit insert $WORKBOT $THRESHOLD1 '' $WEBID cycle -d medium -w $MEDIUM
+
+  BALANCE=$(credit balance $WEBID -d medium -w $MEDIUM)
+
+  if [[ $BALANCE -ge $THRESHOLD2 ]]
+  then
+    credit insert $WEBID $THRESHOLD2 '' $WORKBOT bubble -d medium -w $MEDIUM
+    credit insert $WORKBOT $THRESHOLD2 '' $WEBID cycle -d large -w $LARGE
+  fi
+
+fi
