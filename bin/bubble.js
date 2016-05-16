8a13663710a591ad65efc5227086e27d8e7b72cb
Melvin Carvalho <melvincarvalho@gmail.com>
2016-05-16 21:21:30 +0200

    First release

---
+++ bin/bubble.js
@@ -0,0 +1,101 @@
+#!/usr/bin/env node
+
+var wc = require('webcredits')
+
+var config = wc.getConfig();
+var sequelize = wc.setupDB(config);
+
+console.log(config)
+
+var source      = 'https://workbot.databox.me/profile/card#me'
+var amount      = 5
+var destination = 'https://melvincarvalho.com/#me'
+var currency    = 'https://w3id.org/cc#bit'
+
+var threshold   = 30
+
+var wallet1 = "https://localhost/wallet/small/wallet#this"
+var wallet2 = "https://localhost/wallet/medium/wallet#this"
+
+
+function createCredit(source, amount, currency, destination) {
+  var credit = {}
+
+  var defaultCurrency = 'https://w3id.org/cc#bit'
+
+  if (source) {
+    credit["https://w3id.org/cc#source"] = source
+  } else {
+    return
+  }
+
+  if (amount) {
+    credit["https://w3id.org/cc#amount"] = amount
+  } else {
+    return
+  }
+
+  if (destination) {
+    credit["https://w3id.org/cc#destination"] = destination
+  } else {
+    return
+  }
+
+  if (currency) {
+    credit["https://w3id.org/cc#currency"] = currency
+  } else {
+    credit["https://w3id.org/cc#currency"] = defaultCurrency
+  }
+
+  return credit
+}
+
+
+var credit1 = createCredit(source, amount, currency, destination)
+var credit2 = createCredit(destination, threshold, currency, source)
+var credit3 = createCredit(source, threshold, currency, destination)
+
+config.wallet = wallet1;
+
+wc.insert(credit1, sequelize, config, function(err, res) {
+   if (!err) {
+     console.log(res)
+
+     // hook 1
+     wc.getBalance(destination, sequelize, config, function(err, res){
+
+       if (!err) {
+         console.log('fetched balance is : ' + res)
+
+         // hook 2
+         if (res >= threshold) {
+           wc.insert(credit2, sequelize, config, function(err, res) {
+              if (!err) {
+                console.log(res)
+
+                // hook3
+                config.wallet = wallet2;
+                wc.insert(credit3, sequelize, config, function(err, res) {
+                   if (!err) {
+                     console.log(res)
+                   } else {
+                     console.error(err)
+                   }
+                 })
+
+              } else {
+                console.error(err)
+              }
+            })
+         }
+
+       } else {
+         console.error(err)
+       }
+
+     })
+
+   } else {
+     console.error(err)
+   }
+})
